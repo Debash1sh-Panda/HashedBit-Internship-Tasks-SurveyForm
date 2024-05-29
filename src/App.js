cec58e899e72f67148f035f00b51666090c5b458
Debash1sh-Panda <debasishpanda2036@gmail.com>
2024-05-29 22:58:31 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -67,15 +67,15 @@ function App() {
             <div className="form-group">
             <label>Rating:</label>
             <div className="rating">
-              <input type="radio" id="star5" name="rating" value="5" onChange={handleChange} />
+              <input type="radio" id="star5" name="rating" value="5" checked={formData.rating === '5'} onChange={handleChange} />
               <label htmlFor="star5" title="5 stars">☆</label>
-              <input type="radio" id="star4" name="rating" value="4" onChange={handleChange} />
+              <input type="radio" id="star4" name="rating" value="4" checked={formData.rating === '4'} onChange={handleChange} />
               <label htmlFor="star4" title="4 stars">☆</label>
-              <input type="radio" id="star3" name="rating" value="3" onChange={handleChange} />
+              <input type="radio" id="star3" name="rating" value="3" checked={formData.rating === '3'} onChange={handleChange} />
               <label htmlFor="star3" title="3 stars">☆</label>
-              <input type="radio" id="star2" name="rating" value="2" onChange={handleChange} />
+              <input type="radio" id="star2" name="rating" value="2" checked={formData.rating === '2'} onChange={handleChange} />
               <label htmlFor="star2" title="2 stars">☆</label>
-              <input type="radio" id="star1" name="rating" value="1" onChange={handleChange} />
+              <input type="radio" id="star1" name="rating" value="1" checked={formData.rating === '1'} onChange={handleChange} />
               <label htmlFor="star1" title="1 star">☆</label>
             </div>
           </div>
